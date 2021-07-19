e6a855d5a76fb94e5cd954aff37df059cc018ba5
Evgeny Izutov <evgeny.izutov@intel.com>
2021-07-19 18:54:30 +0300

    Minor

--- configs/_base_/datasets/ade20k.py
+++ configs/_base_/datasets/ade20k.py
@@ -1,13 +1,13 @@
 # dataset settings
 dataset_type = 'ADE20KDataset'
 data_root = 'data/ade/ADEChallengeData2016'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_scale = (2048, 512)
 crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
-    dict(type='Resize', img_scale=(2048, 512), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 512),
+        img_scale=img_scale,
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
@@ -29,7 +29,8 @@ test_pipeline = [
             dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
-        ])
+        ]
+    )
 ]
 data = dict(
     samples_per_gpu=4,
@@ -39,16 +40,20 @@ data = dict(
         data_root=data_root,
         img_dir='images/training',
         ann_dir='annotations/training',
-        pipeline=train_pipeline),
+        pipeline=train_pipeline
+    ),
     val=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='images/validation',
         ann_dir='annotations/validation',
-        pipeline=test_pipeline),
+        pipeline=test_pipeline
+    ),
     test=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='images/validation',
         ann_dir='annotations/validation',
-        pipeline=test_pipeline))
+        pipeline=test_pipeline
+    )
+)
--- configs/_base_/datasets/kvasir.py
+++ configs/_base_/datasets/kvasir.py
@@ -4,12 +4,16 @@ data_root = 'data/kvasir'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 img_scale = (768, 768)
 crop_size = (640, 640)
+# img_scale = (544, 544)
+# crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0), keep_ratio=False),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0.5),
+    dict(type='RandomRotate', prob=0.5, degree=90, pad_val=0, seg_pad_val=255),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='RandomFlip', prob=0.5, direction='vertical'),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
