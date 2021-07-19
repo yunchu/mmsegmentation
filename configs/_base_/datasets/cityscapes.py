79e7404df7f108c5a16614c792d152e9abd8215e
Evgeny Izutov <evgeny.izutov@intel.com>
2021-07-19 17:06:16 +0300

    Minor

--- configs/_base_/datasets/cityscapes.py
+++ configs/_base_/datasets/cityscapes.py
@@ -2,11 +2,12 @@
 dataset_type = 'CityscapesDataset'
 data_root = 'data/cityscapes/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_scale = (2048, 1024)
 crop_size = (512, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -19,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
+        img_scale=img_scale,
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
--- configs/_base_/datasets/pascal_voc12.py
+++ configs/_base_/datasets/pascal_voc12.py
@@ -1,13 +1,13 @@
 # dataset settings
 dataset_type = 'PascalVOCDataset'
 data_root = 'data/VOCdevkit/VOC2012'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_scale = (2048, 512)
 crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(2048, 512), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 512),
+        img_scale=img_scale,
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
@@ -41,18 +41,22 @@ data = dict(
         img_dir='JPEGImages',
         ann_dir='SegmentationClass',
         split='ImageSets/Segmentation/train.txt',
-        pipeline=train_pipeline),
+        pipeline=train_pipeline
+    ),
     val=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='JPEGImages',
         ann_dir='SegmentationClass',
         split='ImageSets/Segmentation/val.txt',
-        pipeline=test_pipeline),
+        pipeline=test_pipeline
+    ),
     test=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='JPEGImages',
         ann_dir='SegmentationClass',
         split='ImageSets/Segmentation/val.txt',
-        pipeline=test_pipeline))
+        pipeline=test_pipeline
+    )
+)
