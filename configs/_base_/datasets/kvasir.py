6a0d6e95e4543d14a3fae16b90394fdbbd2f6fd2
Evgeny Izutov <evgeny.izutov@intel.com>
2021-07-22 13:21:42 +0300

    Fixes

--- configs/_base_/datasets/kvasir.py
+++ configs/_base_/datasets/kvasir.py
@@ -9,12 +9,12 @@ train_pipeline = [
     dict(type='LoadAnnotations'),
     dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0), keep_ratio=False),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='RandomRotate', prob=0.5, degree=90, pad_val=0, seg_pad_val=255),
     dict(type='RandomFlip', prob=0.5, direction='horizontal'),
     dict(type='RandomFlip', prob=0.5, direction='vertical'),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='RandomRotate', prob=0.5, degree=90, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
