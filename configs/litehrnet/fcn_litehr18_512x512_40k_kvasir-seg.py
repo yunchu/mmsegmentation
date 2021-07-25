a6cc6b7ef6f9691a473679eb08e611b651fe52b2
Evgeny Izutov <evgeny.izutov@intel.com>
2021-07-25 22:02:57 +0300

    Minor

--- configs/litehrnet/fcn_litehr18_512x512_40k_kvasir-seg.py
+++ configs/litehrnet/fcn_litehr18_512x512_40k_kvasir-seg.py
@@ -22,7 +22,7 @@ model = dict(
         sampler_loss_idx=0,
         loss_decode=[
             dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            dict(type='BoundaryLoss', loss_weight=0.01)
+            dict(type='BoundaryLoss', loss_weight=0.05)
         ]
     )
 )
