85e5c3b2954429e2a419c33b688313e00ebe3921
Evgeny Izutov <evgeny.izutov@intel.com>
2021-08-18 15:33:19 +0300

    Minor

--- configs/litehrnet/fcn_litehr18_512x1024_80k_cityscapes.py
+++ configs/litehrnet/fcn_litehr18_512x1024_80k_cityscapes.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/fcn_litehr18.py', '../_base_/datasets/cityscapes.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_cos_160k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_cos_80k.py'
 ]
 
 norm_cfg = dict(type='SyncBN', requires_grad=True)
