bcc16ecdd50246b3a1751011fdb50160ad7e2f4a
Evgeny Izutov <evgeny.izutov@intel.com>
2021-08-19 10:52:07 +0300

    Added SepFCN config

---
+++ configs/litehrnet/sep_fcn_litehr18_512x512_20k_kvasir-seg.py
@@ -0,0 +1,33 @@
+_base_ = [
+    '../_base_/models/fcn_litehr18.py', '../_base_/datasets/kvasir.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_cos_20k.py'
+]
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    decode_head=dict(
+        type='DepthwiseSeparableFCNHead',
+        in_channels=40,
+        in_index=0,
+        channels=40,
+        input_transform=None,
+        kernel_size=1,
+        num_convs=2,
+        concat_input=False,
+        dropout_ratio=-1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        sampler=dict(type='MaxPoolingPixelSampler', ratio=0.25, p=1.7),
+        sampler_loss_idx=0,
+        loss_decode=[
+            dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, loss_jitter_prob=0.01),
+        ]
+    ),
+    train_cfg=dict(
+        mix_loss=dict(enable=True, weight=0.1)
+    ),
+)
+evaluation = dict(
+    metric='mDice',
+)
