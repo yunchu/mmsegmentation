3312d903e482dfec11e60353543e87a44f0adef8
Evgeny Izutov <evgeny.izutov@intel.com>
2021-09-02 12:56:33 +0300

    Changed the end scale

--- configs/litehrnet/fcn_litehr18_512x512_40k_kvasir-seg_amsoftmax.py
+++ configs/litehrnet/fcn_litehr18_512x512_40k_kvasir-seg_amsoftmax.py
@@ -24,7 +24,7 @@ model = dict(
                  scale_cfg=dict(
                      type='PolyScalarScheduler',
                      start_scale=30,
-                     end_scale=5,
+                     end_scale=10,
                      num_iters=30000,
                      power=1.2
                  ),
