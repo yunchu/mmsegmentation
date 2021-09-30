158da53b64e2faa5357ae769fb7fe15922ed6441
Evgeny Izutov <evgeny.izutov@intel.com>
2021-09-30 16:30:34 +0300

    Minor

--- mmseg/apis/ote/apis/segmentation/__init__.py
+++ mmseg/apis/ote/apis/segmentation/__init__.py
@@ -1,10 +1,4 @@
-from .config_utils import (
-                           # config_from_string,
-                           # config_to_string,
-                           patch_config,
-                           # prepare_for_testing,
-                           # prepare_for_training,
-                           # save_config_to_file,
+from .config_utils import (patch_config,
                            set_hyperparams,
                            set_values_as_default)
 from .configuration import OTESegmentationConfig
@@ -23,12 +17,3 @@ __all__ = [
     'OTESegmentationTask',
     'OpenVINOSegmentationTask',
 ]
-
-# __all__ = [
-#     config_from_string,
-#     config_to_string,
-#     patch_config,
-#     prepare_for_testing,
-#     prepare_for_training,
-#     save_config_to_file,
-#     ]
