a4678b467d63ff31593bcb1c73817df7eca1daac
Evgeny Izutov <evgeny.izutov@intel.com>
2021-10-21 12:53:05 +0300

    Added config for HRF

---
+++ configs/litehrnet/ocr_litehr18s_256x256_40k_hrf_amsoftmax.py
@@ -0,0 +1,133 @@
+_base_ = [
+    '../_base_/models/fcn_litehr18.py', '../_base_/datasets/hrf.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_step_40k_ml.py'
+]
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='CascadeEncoderDecoder',
+    num_stages=2,
+    backbone=dict(
+        type='LiteHRNet',
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        extra=dict(
+            stem=dict(
+                stem_channels=32,
+                out_channels=32,
+                expand_ratio=1,
+                strides=(2, 2),
+                extra_stride=True,
+                input_norm=False,
+            ),
+            num_stages=3,
+            stages_spec=dict(
+                num_modules=(2, 4, 2),
+                num_branches=(2, 3, 4),
+                num_blocks=(2, 2, 2),
+                module_type=('LITE', 'LITE', 'LITE'),
+                with_fuse=(True, True, True),
+                reduce_ratios=(8, 8, 8),
+                num_channels=(
+                    (40, 80),
+                    (40, 80, 160),
+                    (40, 80, 160, 320),
+                )
+            ),
+            out_modules=dict(
+                conv=dict(
+                    enable=False,
+                    channels=576
+                ),
+                position_att=dict(
+                    enable=False,
+                    key_channels=128,
+                    value_channels=320,
+                    psp_size=(1, 3, 6, 8),
+                ),
+                local_att=dict(
+                    enable=False
+                )
+            ),
+            out_aggregator=dict(
+                enable=True
+            ),
+            add_input=False
+        )
+    ),
+    decode_head=[
+        dict(type='FCNHead',
+             in_channels=40,
+             in_index=0,
+             channels=40,
+             input_transform=None,
+             kernel_size=1,
+             num_convs=0,
+             concat_input=False,
+             dropout_ratio=-1,
+             num_classes=2,
+             norm_cfg=norm_cfg,
+             align_corners=False,
+             enable_out_norm=False,
+             loss_decode=[
+                 dict(type='CrossEntropyLoss',
+                      use_sigmoid=False,
+                      loss_jitter_prob=0.01,
+                      sampler=dict(type='MaxPoolingPixelSampler', ratio=0.25, p=1.7),
+                      loss_weight=1.0),
+             ]),
+        dict(type='OCRHead',
+             in_channels=40,
+             in_index=0,
+             channels=40,
+             ocr_channels=40,
+             out_act_cfg=None,
+             sep_conv=True,
+             input_transform=None,
+             dropout_ratio=-1,
+             num_classes=2,
+             norm_cfg=norm_cfg,
+             align_corners=False,
+             enable_out_norm=True,
+             loss_decode=[
+                 dict(type='AMSoftmaxLoss',
+                      scale_cfg=dict(
+                          type='PolyScalarScheduler',
+                          start_scale=30,
+                          end_scale=5,
+                          num_iters=30000,
+                          power=1.2
+                      ),
+                      margin_type='cos',
+                      margin=0.5,
+                      gamma=0.0,
+                      t=1.0,
+                      target_loss='ce',
+                      pr_product=False,
+                      conf_penalty_weight=dict(
+                          type='PolyScalarScheduler',
+                          start_scale=0.2,
+                          end_scale=0.15,
+                          num_iters=20000,
+                          power=1.2
+                      ),
+                      loss_jitter_prob=0.01,
+                      border_reweighting=False,
+                      sampler=dict(type='MaxPoolingPixelSampler', ratio=0.25, p=1.7),
+                      loss_weight=1.0),
+             ]),
+    ],
+    train_cfg=dict(
+        mix_loss=dict(enable=False, weight=0.1)
+    ),
+    test_cfg=dict(
+        mode='slide',
+        crop_size=(1024, 1024),
+        stride=(680, 680)
+    )
+)
+evaluation = dict(
+    metric='mDice',
+)
+
+find_unused_parameters = True
