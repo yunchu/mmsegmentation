22e5d75fe2507c0fa9e119e181bb6d5a801a7a6a
aiarkinx <antonx.iarkin@intel.com>
2021-12-06 11:50:52 +0300

    rm imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,8 @@
 import e2e.fixtures
 
 from e2e.conftest_utils import * # noqa
-from e2e.conftest_utils import pytest_addoption as _e2e_pytest_addoption # noqa
-from e2e import config # noqa
 from e2e.utils import get_plugins_from_packages
 pytest_plugins = get_plugins_from_packages([e2e])
-
 
 def pytest_addoption(parser):
     parser.addoption('--dataset-definitions', action='store', default=None,
