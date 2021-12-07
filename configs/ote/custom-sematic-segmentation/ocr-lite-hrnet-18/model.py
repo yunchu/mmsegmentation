83353463a2c1c18afaeab2199da37e334ffeac7b
Evgeny Izutov <evgeny.izutov@intel.com>
2021-12-07 22:38:18 +0300

    Updated template

--- configs/ote/custom-sematic-segmentation/ocr-lite-hrnet-18/model.py
+++ configs/ote/custom-sematic-segmentation/ocr-lite-hrnet-18/model.py
@@ -55,17 +55,17 @@ model = dict(
                 )
             ),
             out_aggregator=dict(
-                enable=True
+                enable=False
             ),
             add_input=False
         )
     ),
     decode_head=[
         dict(type='FCNHead',
-             in_channels=40,
-             in_index=0,
+             in_channels=[40, 80, 160, 320],
+             in_index=[0, 1, 2, 3],
+             input_transform='multiple_select',
              channels=40,
-             input_transform=None,
              kernel_size=1,
              num_convs=0,
              concat_input=False,
@@ -73,6 +73,7 @@ model = dict(
              num_classes=2,
              norm_cfg=norm_cfg,
              align_corners=False,
+             enable_aggregator=True,
              enable_out_norm=False,
              loss_decode=[
                  dict(type='CrossEntropyLoss',
@@ -82,16 +83,17 @@ model = dict(
                       loss_weight=1.0),
              ]),
         dict(type='OCRHead',
-             in_channels=40,
-             in_index=0,
+             in_channels=[40, 80, 160, 320],
+             in_index=[0, 1, 2, 3],
+             input_transform='multiple_select',
              channels=40,
              ocr_channels=40,
              sep_conv=True,
-             input_transform=None,
              dropout_ratio=-1,
              num_classes=2,
              norm_cfg=norm_cfg,
              align_corners=False,
+             enable_aggregator=True,
              enable_out_norm=True,
              loss_decode=[
                  dict(type='AMSoftmaxLoss',
@@ -124,7 +126,15 @@ model = dict(
              ]),
     ],
     train_cfg=dict(
-        mix_loss=dict(enable=False, weight=0.1)
+        mix_loss=dict(
+            enable=False,
+            weight=0.1
+        ),
+        loss_reweighting=dict(
+            weights={'decode_0.loss_seg': 0.9,
+                     'decode_1.loss_seg': 1.0},
+            momentum=0.1
+        ),
     ),
     test_cfg=dict(
         mode='whole',
@@ -136,10 +146,10 @@ find_unused_parameters = True
 
 # optimizer
 optimizer = dict(
-    type='SGD',
+    type='Adam',
     lr=1e-3,
-    momentum=0.9,
-    weight_decay=0.0005
+    eps=1e-08,
+    weight_decay=0.0
 )
 optimizer_config = dict(
     grad_clip=dict(
