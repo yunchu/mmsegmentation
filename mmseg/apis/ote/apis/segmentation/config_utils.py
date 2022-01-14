1ea17a53d36def2d441e962de9cd1a7cb80d6c6f
Ilya Krylov <ilya.krylov@intel.com>
2022-01-14 15:39:15 +0300

    TMP

--- mmseg/apis/ote/apis/segmentation/config_utils.py
+++ mmseg/apis/ote/apis/segmentation/config_utils.py
@@ -85,6 +85,7 @@ def patch_config(config: Config,
 
     if 'evaluation' not in config:
         config.evaluation = ConfigDict()
+    config.evaluation['statistic_based'] = True
     evaluation_metric = config.evaluation.get('metric')
     if evaluation_metric is not None:
         config.evaluation.save_best = evaluation_metric
